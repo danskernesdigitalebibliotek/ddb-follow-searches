a641ed1a374e8d742c732aa54f518b92a3aa8ce0
Thomas Fini Hansen <xen@xen.dk>
2019-10-03 08:23:59 +0200

    Make authentication feature pass

---
+++ app/Http/Controllers/SearchesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller;
+
+class SearchesController extends Controller
+{
+    public function get(string $listId)
+    {
+        return [];
+    }
+}
