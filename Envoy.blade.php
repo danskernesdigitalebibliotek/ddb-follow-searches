5a7410f97edddc0b9a8a22a9ff68a63cfb172193
Mathias Larsen <mathias@reload.dk>
2019-10-17 11:52:59 +0200

    Simplify deployment script
    FSS-17

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,25 +1,24 @@
 @servers(['web' => ['ddbdrift@followsearches.dandigbib.org -A -p 222']])
 
+@setup
+  $dir = '~/public_html/checkouts/stage';
+
+  if (!$branch) {
+    $branch = 'develop';
+  }
+
+  if ($env === 'prod') {
+    $dir = '~/public_html/checkouts/prod';
+    $branch = 'master';
+  }
+@endsetup
+
 @task('deploy', ['on' => ['web']])
-    @if ($env === 'prod')
-      cd ~/public_html/checkouts/prod
-      @if (!$branch)
-        git fetch
-        git checkout -f origin/master
-      @endif
-    @endif
-    @if ($env === 'stage')
-      cd ~/public_html/checkouts/stage
-      @if (!$branch)
-        git fetch
-        git checkout -f origin/develop
-      @endif
-    @endif
-    @if ($branch)
-      git fetch
-      git checkout -f origin/{{$branch}}
-    @endif
-    php -dallow_url_fopen=1 ~/bin/composer self-update
-    php -dallow_url_fopen=1 ~/bin/composer install --prefer-dist --no-dev
-    php ./artisan migrate
+  cd {{ $dir }}
+  git fetch
+  git reset --hard origin/{{ $branch }}
+
+  php -dallow_url_fopen=1 ~/bin/composer self-update
+  php -dallow_url_fopen=1 ~/bin/composer install --prefer-dist --no-dev
+  php ./artisan migrate
 @endtask
