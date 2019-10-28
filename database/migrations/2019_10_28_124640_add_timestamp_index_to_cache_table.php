52573ab912804afd7a4030d2b2f33e84719b9a44
Thomas Fini Hansen <xen@xen.dk>
2019-10-28 14:04:36 +0100

    Fix drop migration

--- database/migrations/2019_10_28_124640_add_timestamp_index_to_cache_table.php
+++ database/migrations/2019_10_28_124640_add_timestamp_index_to_cache_table.php
@@ -26,7 +26,7 @@ class AddTimestampIndexToCacheTable extends Migration
     public function down()
     {
         Schema::table('cache', function (Blueprint $table) {
-            $table->dropIndex('timestamp');
+            $table->dropIndex(['timestamp']);
         });
     }
 }
