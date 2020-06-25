950172f753cc8f9bab0d131c7d3b45ecf258409f
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-25 10:07:19 +0200

    DDBTEAM-553: Added image build and helm charts

---
+++ infrastructure/docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+APP_VERSION=develop
+VERSION=latest
+
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/follow-searches:${VERSION} --file="follow-searches/Dockerfile" follow-searches
+docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/follow-searches-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+
+#docker push danskernesdigitalebibliotek/follow-searches:${VERSION}
+#docker push danskernesdigitalebibliotek/follow-searches-nginx:${VERSION}
