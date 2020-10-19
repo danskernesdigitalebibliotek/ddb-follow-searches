c8d166f46444f5b553f0f59013707f9d1985464f
Jesper Kristensen <cableman@linuxdev.dk>
2020-10-19 15:56:25 +0200

    DDBTEAM-561: Updated helm chart

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -6,5 +6,5 @@ VERSION=latest
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/follow-searches:${VERSION} --file="follow-searches/Dockerfile" follow-searches
 docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/follow-searches-nginx:${VERSION} --file="nginx/Dockerfile" nginx
 
-#docker push danskernesdigitalebibliotek/follow-searches:${VERSION}
-#docker push danskernesdigitalebibliotek/follow-searches-nginx:${VERSION}
+docker push danskernesdigitalebibliotek/follow-searches:${VERSION}
+docker push danskernesdigitalebibliotek/follow-searches-nginx:${VERSION}
